ae3cfbeac8cb264618389242426bfb60bb967608
FernandoViegas <fndtech2020@gmail.com>
2023-06-30 21:38:46 -0300

    Show one article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all # o @ serve para mostrar que a variavel esta disponivel para nossa view
   end
+
+  def show
+    @article = Article.find(params[:id]) # o @ serve para mostrar que a variavel esta disponivel para nossa view
+  end
 end
